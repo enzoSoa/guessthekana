d87053c0b7affa1cf0a79423e9b1e3d907de8a85
Enzo Ayrton Soares <enzoayrsoares@gmail.com>
2025-08-16 14:44:15 +0200

    Don't piss them off

---
+++ src/components/Viewer/Viewer.tsx
@@ -0,0 +1,17 @@
+import { useReducer } from 'react';
+import { Eye } from '..';
+import './Viewer.css';
+import { viewerReducer } from './reducer';
+
+export function Viewer() {
+    const [{angriness}, dispatch] = useReducer(viewerReducer, {angriness: 0});
+
+    const handleEyePoked = () => dispatch({type: 'get-angrier', data: undefined})
+
+    return <div className='viewer'>
+        <div className='viewer-sight' style={{'--angriness': angriness}}>
+            <Eye onPoke={handleEyePoked}/>
+            <Eye onPoke={handleEyePoked}/>
+        </div>
+    </div>;
+}
