293edce15f4e87ee559cff97ea964b03f4ed2fce
Enzo Ayrton Soares <enzoayrsoares@gmail.com>
2025-08-24 15:58:32 +0200

    Twiking manifest

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+  });
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
